11302c7d6d7ea04aaead6bbc8819d5a90d8e4ce2
JLoloCkik <157353695+JLoloCkik@users.noreply.github.com>
2024-02-10 17:35:09 +0100

    :

---
+++ 12_ha akkor gyors lefolyás/index.js
@@ -0,0 +1,31 @@
+// let age = 12
+// let message = age >= 18 ? "You are an adult" : "You are minor"
+// console.log(message)
+
+
+
+// let message 
+
+// if (age >= 18) {
+//     message = "You are an adult"
+// }
+
+// else {
+//     message = "You are minor"
+// }
+
+
+
+// let time = 16;
+// let greeting = time < 12 ? "gm" : "afternon"
+// console.log(greeting)
+
+
+// let students = false;
+// let message = students ? "You are students" : "You are not students"
+// console.log(message)
+
+let amount = 120
+let disount = amount >= 100 ? 10 : 0
+console.log(`Your total is $${amount - amount* (disount/100)}`)
+
