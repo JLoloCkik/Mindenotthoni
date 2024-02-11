bc4d8dda7eebfeb6752e9acff02a45a9180aacca
JLoloCkik <157353695+JLoloCkik@users.noreply.github.com>
2024-02-11 16:16:54 +0100

    a

--- 24_/index.js
+++ 24_/index.js
@@ -1,2 +1,29 @@
+const text = document.getElementById('text');
+const celsius = document.getElementById('celsius');
+const faren = document.getElementById('faren');
+const res = document.getElementById('res');
 
-function convert 
+let temp
+
+function convert(){
+    if(faren.checked){
+        // res.textContent = "A farenhait választotottad"
+        temp = Number(text.value)
+        temp = temp * 9/5 + 32
+        res.textContent = temp.toFixed(1) + "°F"
+
+    }
+    else if(celsius.checked){
+        // res.textContent = "A Celsiust választotottad"
+        temp = Number(text.value)
+        temp = (temp - 32) * (5/9)
+        res.textContent = temp.toFixed(1) + "°C"
+
+
+    }
+    else {
+        res.textContent = "Válasz!!"
+
+    }
+
+}
